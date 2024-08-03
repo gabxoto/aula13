885f1203e462799e27f1cef28d7dfed1d2e1876e
Gabriel Peixoto <peixoto.gabgab@outlook.com>
2024-08-02 23:28:19 -0300

    Add files via upload

---
+++ ambiente.js
@@ -0,0 +1,21 @@
+/*2° teste - Faz  o bloco primeiro para depois fazer o teste*/
+var c = 1 
+do {
+   console.log('Tudo bem?')
+   c++ //c = c + 1 - c recebe c mais um 
+} while (c <= 6)
+
+/*1° TESTE - COM WHILE / EXECUTA O TESTE LOGICO NO INICIO 
+var c = 1 
+while(c <= 6){
+   console.log('Tudo bem?')
+   c++ //c = c + 1 - c recebe c mais um 
+}*/
+
+
+/* console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')*/
